223213939b8297f63ed27d2962df5c106c09bc64
Joshua Weiss <jtw@joshuatweiss.com>
2018-02-27 15:59:56 -0500

    small fix

--- build_flash.sh
+++ build_flash.sh
@@ -6,7 +6,7 @@ elif [ $1 = "infinity_ergodox_right" ]; then
   SERIAL="mk20dx256vlh7" make ergodox_infinity:josh:dfu-util
 elif [ $1 = "ergodox_ez" ]; then
   docker run -v $PWD:/qmk qmk make clean ; make ergodox_ez:josh
-  teensy_loader_cli -mmcu=atmega32u4 -w $2
+  teensy_loader_cli -mmcu=atmega32u4 -w ergodox_ez_josh.hex
 else
   echo Invalid Keyboard;
   exit 1;
